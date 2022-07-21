4e09c0b3e0894c05bbaa3388123a9644f1ce3aff
Ramom Ferraz <ramomferraz@hotmail.com>
2022-07-21 02:09:20 -0300

    corrigido a identação da página app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
-import Dicas from "./src/controllers/Dicas.js"
+import Dicas from "./src/controllers/Dicas.js";
 
-const port = process.env.PORT || 3000
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.listen(port, () => {
     console.log(`Servidor online em http://localhost:${port}`)
